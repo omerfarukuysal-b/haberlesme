62d2a1c018485abd22bc17b18ad95d0416a60b56
omerfarukuysal-b <omerfaruk.uysal@baykartech.com>
2026-01-02 15:06:08 +0300

    fix

--- cluster/master/node_registry.h
+++ cluster/master/node_registry.h
@@ -5,7 +5,6 @@
 #include <string>
 #include <unordered_map>
 #include "../common/mini_json.h"
-
 struct NodeState {
   uint8_t id = 0;
   std::string lastHeartbeatJson;   // komple heartbeat JSON
@@ -21,7 +20,7 @@ public:
 
   bool get_last_addr(uint8_t targetId, sockaddr_in& out) const;
 
-  bool get_telemetry_json(uint8_t id, std::string& outTelemetry, uint64_t& outAgeMs) const;
+  bool get_telemetry_json(uint8_t id, std::string& outTelemetry, uint64_t& outAgeMs, uint64_t nowMs) const;
 
   std::string to_nodes_json(uint64_t nowMs) const;
 
